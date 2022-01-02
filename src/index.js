7662035df0d5062da096afe75362d42e7c5b2add
YonatanZel <aartemiyev@gmail.com>
2022-01-02 22:22:06 +0200

    commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+console.log("script launched");
+let detailsTitle = document.querySelector(".details-title");
+let detailsImage = document.querySelector(".details-image");
+let anchors = document.querySelectorAll(".thumbnails-anchor");//all html elements belonging class thumbnails-anchor
+for(let i = 0; i < anchors.length; i++)
+{
+    anchors[i].addEventListener("click", function(event)
+    {
+        event.preventDefault();//canceling default behavior of anchor element hitting
+        setDetails(anchors[i]);//setDetails function call with passing reference to appropriate anchor
+    })
+}
+function setDetails(anchor)
+{
+    detailsImage.setAttribute("src",anchor.getAttribute("href"));
+    detailsTitle.textContent = anchor.getAttribute("data-details-title");
+}
