321469009acf4c7c8128ca269b342c7862b9dcb0
YonatanZel <aartemiyev@gmail.com>
2022-01-02 22:56:54 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -13,5 +13,6 @@ for(let i = 0; i < anchors.length; i++)
 function setDetails(anchor)
 {
     detailsImage.setAttribute("src",anchor.getAttribute("href"));
-    detailsTitle.textContent = anchor.getAttribute("data-details-title");
+    detailsTitle.textContent = anchor.querySelector(".thumbnails-title").textContent + ": " + anchor.getAttribute ("data-details-title");
+
 }
